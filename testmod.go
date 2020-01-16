0d24ca99b64c41adf1ac563dcc6aaf4e791d6858
AlvinFu <AlvinFu@boyaa.com>
2020-01-16 11:17:02 +0800

    fix: 添加新的版本

--- testmod.go
+++ testmod.go
@@ -2,8 +2,12 @@ package testmod
 
 import "fmt"
 
-func Printf(args ...interface{}) {
-	fmt.Println("welcome")
+func Printf(args ...interface{}) error {
+	fmt.Println("Welcome !")
+	if len(args) <= 0 {
+		return fmt.Errorf("args err!")
+	}
 	fmt.Println(args...)
 	fmt.Println("See you!")
+	return nil
 }
